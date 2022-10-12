c92f0a679986e1d40e99657eb17afbeeb3a70ac0
Abrasha94 <topdisplay@mail.ru>
2022-10-12 11:02:42 +0300

    PR

--- src/main/java/com/modsen/balancefromcard/rest/BalanceControllerV1.java
+++ src/main/java/com/modsen/balancefromcard/rest/BalanceControllerV1.java
@@ -6,7 +6,10 @@ import com.modsen.balancefromcard.service.BalanceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/balance/")
